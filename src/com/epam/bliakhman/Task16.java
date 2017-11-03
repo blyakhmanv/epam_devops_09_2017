8bc7133a41fab39b0eddc31ab1561c17ed368f7b
Vladimir_Bliakhman <vladimir_bliakhman@epam.com>
2017-11-03 09:26:56 +0300

    Task16 vert and hor shifts, not tests yet

--- src/com/epam/bliakhman/Task16.java
+++ src/com/epam/bliakhman/Task16.java
@@ -8,7 +8,9 @@ public class Task16 {
 
         int[][] array = new int[][]{{0,5},{1,5},{2,5},{3,5}};
         System.out.println ( Arrays.deepToString ( array ) );
-        verticalCyclicShift ( array, -2 );
+        array = verticalCyclicShift ( array, 0 );
+        System.out.println ( Arrays.deepToString ( array ) );
+        array = horizontalCyclicShift ( array, 1 );
         System.out.println ( Arrays.deepToString ( array ) );
     }
 
@@ -18,7 +20,8 @@ public class Task16 {
      * @param numberShifts Положительное значение соответствует циклическому сдвигу вниз, отрицательное - вверх.
      * @return Новая матрица, содержащая результат применения операции.
      */
-    public static void verticalCyclicShift (int[][] matrix, int numberShifts) {
+
+    public static int[][] verticalCyclicShift (int[][] matrix, int numberShifts) {
         if (matrix == null || matrix.length==0) {
             //    if (matrix == null || matrix.length==0 || order < 0) {
             throw new IllegalArgumentException("Illegal argument!");
@@ -30,7 +33,28 @@ public class Task16 {
         reverse(matrix, 0, a-1);
         reverse(matrix, a, matrix.length-1);
         reverse(matrix, 0, matrix.length-1);
+        return matrix;
+    }
+    /**
+     * Выполняет циклический сдвиг матрицы на указанное количество позиций по горизонтали.
+     * @param matrix Исходная матрица.
+     * @param numberShifts Положительное значение соответствует циклическому сдвигу вправо, отрицательное - влево.
+     * @return Новая матрица, содержащая результат применения операции.
+     */
 
+    public static int[][] horizontalCyclicShift(int[][] matrix, int numberShifts) {
+        matrix = verticalCyclicShift ( rotate(matrix), numberShifts );
+        return rotate(matrix);
+    }
+
+    private static int[][] rotate (int[][] matrix) {
+        int[][] result = new int[matrix[0].length][matrix.length];
+        for ( int i = 0 ; i <matrix.length  ; i++ ) {
+            for ( int j = 0 ; j <matrix[i].length  ; j++ ) {
+                result[j][i] = matrix[i][j];
+            }
+        }
+        return result;
     }
 
     public static void reverse(int[][] arr, int left, int right){
@@ -46,16 +70,6 @@ public class Task16 {
         }
     }
 
-    /**
-     * Выполняет циклический сдвиг матрицы на указанное количество позиций по горизонтали.
-     * @param matrix Исходная матрица.
-     * @param numberShifts Положительное значение соответствует циклическому сдвигу вправо, отрицательное - влево.
-     * @return Новая матрица, содержащая результат применения операции.
-     */
-    public int[][] horizontalCyclicShift(int[][] matrix, int numberShifts) {
-        // TODO решение задачи
-        return null;
-    }
 
 }
 
