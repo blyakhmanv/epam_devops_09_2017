73a79957be5d95fc3fd2227397a0b9b34549f2ea
Vladimir_Bliakhman <vladimir_bliakhman@epam.com>
2017-11-03 10:29:16 +0300

    Task

--- src/com/epam/bliakhman/Task16.java
+++ src/com/epam/bliakhman/Task16.java
@@ -4,23 +4,12 @@ import java.util.Arrays;
 
 public class Task16 {
 
-    public static void main(String[] args) {
-
-        int[][] array = new int[][]{{0,5},{1,5},{2,5},{3,5}};
-        System.out.println ( Arrays.deepToString ( array ) );
-        array = verticalCyclicShift ( array, 0 );
-        System.out.println ( Arrays.deepToString ( array ) );
-        array = horizontalCyclicShift ( array, 1 );
-        System.out.println ( Arrays.deepToString ( array ) );
-    }
-
     /**
      * Выполняет циклический сдвиг матрицы на указанное количество позиций по вертикали.
      * @param matrix Исходная матрица.
      * @param numberShifts Положительное значение соответствует циклическому сдвигу вниз, отрицательное - вверх.
      * @return Новая матрица, содержащая результат применения операции.
      */
-
     public static int[][] verticalCyclicShift (int[][] matrix, int numberShifts) {
         if (matrix == null || matrix.length==0) {
             //    if (matrix == null || matrix.length==0 || order < 0) {
@@ -35,13 +24,13 @@ public class Task16 {
         reverse(matrix, 0, matrix.length-1);
         return matrix;
     }
+
     /**
      * Выполняет циклический сдвиг матрицы на указанное количество позиций по горизонтали.
      * @param matrix Исходная матрица.
      * @param numberShifts Положительное значение соответствует циклическому сдвигу вправо, отрицательное - влево.
      * @return Новая матрица, содержащая результат применения операции.
      */
-
     public static int[][] horizontalCyclicShift(int[][] matrix, int numberShifts) {
         matrix = verticalCyclicShift ( rotate(matrix), numberShifts );
         return rotate(matrix);
@@ -57,7 +46,7 @@ public class Task16 {
         return result;
     }
 
-    public static void reverse(int[][] arr, int left, int right){
+    private static void reverse(int[][] arr, int left, int right){
         if(arr == null || arr.length == 1)
             return;
 
